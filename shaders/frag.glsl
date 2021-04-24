1bd0a363c1887c007bca2fd9c0faf30e16ef64cb
Billy Stevens <contact@wasv.me>
2021-04-24 19:23:07 -0400

    Reformats shader.

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -11,8 +11,11 @@ void main()
     vec2 pos = (gl_FragCoord.xy / v2Resolution.xy - vec2(0.5,0.5)) / vec2(ar, 1.0);
     pos += vec2( cos(fGlobalTime/1000), sin(fGlobalTime/1000) ) / 4;
 
-    vec2 uv1 = step(vec2(-0.1,-0.5), pos) * step(pos, vec2(0.1,0.5)) * smoothstep(0.5, -0.1, abs(pos));
-    vec2 uv2 = step(vec2(-0.5/ar,-0.1), pos) * step(pos, vec2(0.5/ar,0.1)) * smoothstep(0.5, -0.1, abs(pos));
+    vec2 uv1 = step(vec2(-0.1,-0.5), pos)    * step(pos, vec2(0.1,0.5))    *
+        smoothstep(0.5, -0.1, abs(pos));
+    vec2 uv2 = step(vec2(-0.5/ar,-0.1), pos) * step(pos, vec2(0.5/ar,0.1)) *
+        smoothstep(0.5, -0.1, abs(pos));
 
-    fragColor = mix(vec4(0.0), vec4(0.0,1.0,0.5,0.0), (uv1.x * uv1.y)/2 + (uv2.x * uv2.y)/2);
+    fragColor = mix(vec4(0.0), vec4(0.0,1.0,0.5,0.0),
+            (uv1.x * uv1.y)/2 + (uv2.x * uv2.y)/2);
 }
